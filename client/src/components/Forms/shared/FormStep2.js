44c1cf6c94fc673e24350802497a9d2acc0ef0a7
WhitneyDluhosh <whitneydluhosh@gmail.com>
2021-02-11 11:34:48 -0800

    baseline code for Access to Technology

---
+++ client/src/components/Forms/shared/FormStep2.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+
+class FormStep2 extends Component {
+        render() {
+            if (this.props.currentStep !== 2) {
+                return null
+            }
+        //Else return step 2
+        return (
+        <div>
+           
+        </div>
+        )
+    }
+}
+
+export default FormStep2
