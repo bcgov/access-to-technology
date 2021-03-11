e7222507b55d7283219fd666fe9049f82d891316
WhitneyDluhosh <whitneydluhosh@gmail.com>
2021-03-10 16:54:39 -0800

    update post to res.OK

--- client/src/constants/form.js
+++ client/src/constants/form.js
@@ -6,10 +6,10 @@ export const FORM_URL = {
 }
 
 if (process.env.NODE_ENV === 'development'){
-    FORM_URL.ProviderIntakeForm = 'http://localhost:8080/api/providerForm'
-    FORM_URL.needEmployeeForm = 'http://localhost:8080/api/needEmployeeForm'
-    FORM_URL.clientForm = 'http://localhost:8080/api/participantForm'
-    FORM_URL.claimForm = 'http://localhost:8080/api/claimForm'
+    FORM_URL.ProviderIntakeForm = 'http://localhost:8000/api/providerForm'
+    FORM_URL.needEmployeeForm = 'http://localhost:8000/api/needEmployeeForm'
+    FORM_URL.clientForm = 'http://localhost:8000/api/participantForm'
+    FORM_URL.claimForm = 'http://localhost:8000/api/claimForm'
 } else if (process.env.NODE_ENV === 'production') {
     FORM_URL.ProviderIntakeForm = '/api/providerForm'
     FORM_URL.needEmployeeForm = '/api/needEmployeeForm'
