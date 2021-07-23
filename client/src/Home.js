fa259f67e0412f3fad715c3835befe2eb1b1870c
Dluhosh <Whitney.Dluhosh@gov.bc.ca>
2021-07-23 13:51:26 -0700

    updated Service Provider guide and added link to youtube video

--- client/src/Home.js
+++ client/src/Home.js
@@ -18,7 +18,7 @@ function Home() {
                                     <li>Is a minimum of four consecutive weeks; and</li>
                                     <li>Is on the list of approved skills training programs.</li>
                                 </ul>
-                                <p>For additional Service Provider Information see the <a href="/PDF/A2TServiceProviderGuide.pdf" target="_blank" rel="noopener noreferrer" >Access to Technology (A2T) Service Provider Guide</a>.</p>
+                                <p>For additional Service Provider Information see the <a href="/PDF/A2TServiceProviderGuide.pdf" target="_blank" rel="noopener noreferrer" >Access to Technology (A2T) Service Provider Guide</a> and the <a href="https://www.youtube.com/watch?v=-uge63dLsOk" target="_blank" rel="noopener noreferrer" >Access to Technology (A2T) Session Video</a>.</p>
                                 <p>In order to submit an application for a client to access the A2T Program, you will need the following: </p>
                                 <ol style={{listStyleType:"decimal"}}>
                                     <li>Training Program start and end dates</li>
